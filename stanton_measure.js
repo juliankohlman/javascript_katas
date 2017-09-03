d58652b30c01516a3b0f02e37e62a47533ae5bca
Julian Kohlman <juliankohlman@gmail.com>
2017-09-02 20:33:33 -0400

    added stantonMeasure 7kyu JS kata

---
+++ stanton_measure.js
@@ -0,0 +1,15 @@
+// The Stanton measure of an array is computed as follows. Count the number of 1s
+// in the array. Let this count be n. The Stanton measure is the number of times
+// that n appears in the array.
+
+// eg:-
+
+//  TheStantonmeasureof[1,4,3,2,1,2,3,2]is3
+
+//  because 1 occurs 2 times in the array and 2 occurs 3 times. Write a function
+// stantonMeasure/StantonMeasure which takes an integer array and returns its
+// Stanton measure.
+
+function stantonMeasure(argument) {
+  // body...
+}
