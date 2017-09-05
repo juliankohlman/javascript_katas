241f4bc1fd6dbe2997e0fafde4d631be3b707b3b
Julian Kohlman <juliankohlman@gmail.com>
2017-09-05 09:38:10 -0400

    largestItem 7kyu JS

---
+++ largestItems.js
@@ -0,0 +1,16 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+function largest(n,xs){
+  return xs.sort(function(a,b){ return b-a; }).slice(0,n).reverse();
+}
+
+// Top voted solution did not have to reverse items at the end
+// Because they sorted them properly
+// function largest(n,xs){
+//   return xs.sort(function(a, b) {return a - b;}).slice(xs.length-n);
+// }
