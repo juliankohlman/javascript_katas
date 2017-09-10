1d19dcca2c0caa0e2e5fd502b0ee9faa5ef09f54
Julian Kohlman <juliankohlman@gmail.com>
2017-09-09 20:09:24 -0400

    generate range of ints 8kyu JS

---
+++ generateRangeOfInts.js
@@ -0,0 +1,31 @@
+// Implement a function named generateRange(min, max, step), which takes three
+// arguments and generates a range of integers from min to max, with the step.
+// The first integer is the minimum value, the second is the maximum of the range
+// and the third is the step. (min < max)
+
+// Task
+
+// Implement a function named
+
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10] , which takes
+// three arguments and generates a range of integers from min to max, with given
+// step. The first is minimum value, second is maximum of range and the third is
+// step.
+
+function generateRange (min, max, step) {
+  var res = [];
+  for (min; min <= max;) {
+    res.push(min);
+    min += step;
+  }
+  return res;
+}
+
+// function generateRange(min, max, step){
+//   let arr = [];
+//   for (let i=min; i<=max; i += step) {
+//     arr.push(i);
+//   }
+//   return arr;
+// }
