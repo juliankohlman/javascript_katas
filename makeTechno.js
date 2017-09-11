54ce074a90e90e9ebb59e2b8463481c63960da3b
Julian Kohlman <juliankohlman@gmail.com>
2017-09-11 18:30:08 -0400

    interweave strings 7kyu JS

--- makeTechno.js
+++ makeTechno.js
@@ -22,4 +22,14 @@ function perc (mins) {
   return [`${Math.round(claps * 2)} kicks`, `${Math.round(claps * 4)} hihats`, `${Math.round(claps)} claps`];
 }
 
+// CW Solution
+// function perc (mins) {
+//   if(typeof mins!=='number'||mins<1||mins>100)return "invalid track time";
+//   var arr=[];
+//   arr.push(Math.round(mins*120)+' kicks');
+//   arr.push(Math.round(mins*240)+' hihats');
+//   arr.push(Math.round(mins*60)+' claps');
+//   return arr;
+// }
+
 perc(6);
