a6b857c5ae937bf51310954b6d458ee505fa4f75
Julian Kohlman <juliankohlman@gmail.com>
2017-09-12 09:11:09 -0400

    sortingArray by another array 7kyu

--- sortingArrays.js
+++ sortingArrays.js
@@ -17,6 +17,20 @@
 // Each element in the array will start with a unique letter and there will only
 // be a single match for each element.
 
-function sortArray(a1, a2) {
-  // SOLVE IT ON PAPER FIRST
+function sortArray (a1, a2) {
+  var sorted = [];
+  for (var word of a1) {
+    sorted.push(a2.find(function (item) {
+      return word.startsWith(item[0]);
+    }));
+  }
+  return sorted;
 }
+
+var a1 = ['giraffe', 'orangutan', 'impala', 'elephant', 'rhino'];
+var a2 = ['rattlesnake', 'eagle', 'geko', 'iguana', 'octopus'];
+sortArray(a1, a2) // ['geko', 'octopus', 'iguana', 'eagle', 'rattlesnake']
+
+var a1 = ['jellyfish', 'koi', 'caribou', 'owl', 'dolphin'];
+var a2 = ['ostrich', 'jaguar', 'deer', 'camel', 'kangaroo'];
+sortArray(a1, a2) // ['jaguar', 'kangaroo', 'camel', 'ostrich', 'deer']
