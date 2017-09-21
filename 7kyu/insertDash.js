6926c784e4c53fb4fb5b80a0bacbfd6fec486c7a
Julian Kohlman <juliankohlman@gmail.com>
2017-09-21 13:32:30 -0400

    insertDash with map could also use reduce

--- 7kyu/insertDash.js
+++ 7kyu/insertDash.js
@@ -2,8 +2,17 @@
 // ('-') between each two odd numbers in num. For example: if num is 454793 the
 // output should be 4547-9-3. Don't count zero as an odd number.
 
-function insertDash(num) {
-   //code me
+function insertDash (num) {
+  let digits = num.toString().split('');
+  let result = digits.map(function (str, i, arr) {
+    if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && i < arr.length - 1) {
+      return str + '-';
+    } else {
+      return str;
+    }
+  }).join('');
+
+  return result;
 }
 
 insertDash(454793)
