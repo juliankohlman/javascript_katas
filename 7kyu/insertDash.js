09ab6ea0aae7b54c4e034096b6ae806900f629c3
Julian Kohlman <juliankohlman@gmail.com>
2017-09-21 12:48:58 -0400

    insertDash 7kyu

---
+++ 7kyu/insertDash.js
@@ -0,0 +1,11 @@
+// Write a function insertDash(num)/InsertDash(int num) that will insert dashes
+// ('-') between each two odd numbers in num. For example: if num is 454793 the
+// output should be 4547-9-3. Don't count zero as an odd number.
+
+function insertDash(num) {
+   //code me
+}
+
+insertDash(454793)
+insertDash(123456)
+insertDash(1003567)
