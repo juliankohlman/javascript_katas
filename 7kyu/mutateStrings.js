6bb3a7e06528b8c3cc4a1ddc9b2c344ae94e8d42
Julian Kohlman <juliankohlman@gmail.com>
2017-09-23 19:23:10 -0400

    mutate strings 7kyu will complete tomorrow

--- 7kyu/mutateStrings.js
+++ 7kyu/mutateStrings.js
@@ -14,7 +14,8 @@
 // turtle ham
 
 function mutateMyStrings (stringOne, stringTwo) {
-
+  // stub / pseudocode plan here (SOLVE IT ON PAPER FIRST)
 }
 
-mutateMyStrings('bubble gum', 'turtle ham'); //'bubble gum\ntubble gum\nturble gum\nturtle gum\nturtle hum\nturtle ham\n'
+mutateMyStrings('bubble gum', 'turtle ham');
+// 'bubble gum\ntubble gum\nturble gum\nturtle gum\nturtle hum\nturtle ham\n'
