d1c819e0a3be7f591849c3f88309075fefba8ca9
Julian Kohlman <juliankohlman@gmail.com>
2017-10-03 10:03:35 -0400

    moveVowels 7kyu

---
+++ 7kyu/moveVowels.js
@@ -0,0 +1,19 @@
+// Given a string as input, move all of its vowel to the end of the string.
+
+// Example input: day Example output: dya
+
+// Example input: apple Example output: pplae
+
+// Note: All input string are of small letters, and the order of the vowels at
+// the end should be the same as they were before.
+
+function moveVowel (input) {
+  const nonVowel = input.split('').filter(i => i.match(/[^aeiou]/));
+  const vowels = input.split('').filter(i => i.match(/[aeiou]/));
+  return nonVowel.concat(vowels).join('');
+}
+
+moveVowel('day') // dya
+moveVowel('apple') // pplae
+moveVowel('peace') // pceae
+moveVowel('maker') // mkrae
