f471d2355267d321f4305f9f611e061f8c7f9c3c
Julian Kohlman <juliankohlman@gmail.com>
2017-10-03 09:19:19 -0400

    indexCaps 7kyu warmup

---
+++ 7kyu/indexCapitalization.js
@@ -0,0 +1,25 @@
+// Given a string and an array of integers representing indices, capitalize all
+// letters at the given indices.
+
+// For example:
+
+// capitalize("abcdef",[1,2,5]) = "aBCdeF"
+
+// capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+
+// The input will be a lowercase string with no spaces and an array of digits.
+
+function capitalize (s, arr) {
+  let result = '';
+  for (let i = 0; i < s.length; i++) {
+    arr.includes(i) ? result += s[i].toUpperCase() : result += s[i];
+  }
+  return result;
+};
+
+capitalize('abcdef', [1, 2, 5]) // 'aBCdeF'
+capitalize('abcdef', [1, 2, 5, 100]) // 'aBCdeF'
+capitalize('codewars', [1, 3, 5, 50]) // 'cOdEwArs'
+capitalize('abracadabra', [2, 6, 9, 10]) // 'abRacaDabRA'
+capitalize('codewarriors', [5]) // 'codewArriors'
+capitalize('indexinglessons', [0]) // 'Indexinglessons'
