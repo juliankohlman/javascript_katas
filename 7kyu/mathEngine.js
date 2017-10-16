a04b4b9e515b6e98c31ce99f91b399e9462d8612
Julian Kohlman <juliankohlman@gmail.com>
2017-10-16 19:56:09 -0400

    Use const instead of let

--- 7kyu/mathEngine.js
+++ 7kyu/mathEngine.js
@@ -1,7 +1,7 @@
 function mathEngine (arr) {
   if (arr === null) return 0;
-  let posNums = arr.filter(i => i >= 0).reduce((a, b) => a * b, 1)
-  let negNums = arr.filter(i => i <= 0).reduce((a, b) => a + b, 0)
+  const posNums = arr.filter(i => i >= 0).reduce((a, b) => a * b, 1)
+  const negNums = arr.filter(i => i <= 0).reduce((a, b) => a + b, 0)
   return arr.length === 0 ? 1 : posNums + negNums;
 }
 
