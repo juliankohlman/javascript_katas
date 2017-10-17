dfd2416b0299da044abd8fe43f670b9d0e2db23f
Julian Kohlman <juliankohlman@gmail.com>
2017-10-16 20:50:32 -0400

    Word to binary 7kyu solution

--- 7kyu/wordToBinary.js
+++ 7kyu/wordToBinary.js
@@ -4,6 +4,16 @@
 
 // Example: 'man' should return [ '01101101', '01100001', '01101110' ]
 
-function wordToBin(str){
-//   return array;
+function wordToBin (str) {
+  const res = [];
+  for (let l of str) {
+    res.push('0' + l.charCodeAt().toString(2))
+  }
+  return res;
 }
+
+wordToBin('man') // [ '01101101', '01100001', '01101110' ]
+wordToBin('AB') // ['01000001', '01000010']
+wordToBin('wecking') // [ '01110111', '01100101', '01100011', '01101011', '01101001', '01101110', '01100111' ]
+wordToBin('Ruby') // [ '01010010', '01110101', '01100010', '01111001' ]
+wordToBin('Yosemite') // [ '01011001', '01101111', '01110011', '01100101', '01101101', '01101001', '01110100', '01100101' ]
