a72b5992eab7be73cf33afd1b75a6b88a45f3f47
Julian Kohlman <juliankohlman@gmail.com>
2017-10-17 20:52:37 -0400

    Repeat strings solution

---
+++ 7kyu/repeatString.js
@@ -0,0 +1,20 @@
+// Write a function hasOneChar returning:
+
+// true if the given string contains the same character repeated all along the
+// string false otherwise. For instance:
+
+// But the purpose of this Kata is to test your creativity. You have to
+// achieve it without using any loops.
+
+// Corner cases: the function hasOneChar should return true if the string
+// contains one single character.
+
+function hasOneChar (s) {
+  return s[0].repeat(s.length) === s;
+}
+
+hasOneChar('a')
+hasOneChar('aaaaa')
+hasOneChar('aaaab')
+hasOneChar('bbbbb')
+hasOneChar('bbabb')
