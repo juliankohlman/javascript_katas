d1b7eb910c13a1f85601314254a5679da0160ab1
Julian Kohlman <juliankohlman@gmail.com>
2017-10-20 19:33:21 -0400

    Zero Plentiful Array 6kyu Stub

---
+++ 6kyu/zeroPlentifulArray.js
@@ -0,0 +1,8 @@
+// An array is called zero-plentiful if it contains at least one 0 and every
+// sequence of 0s is of length at least 4. Your task is to return the number of
+// zero sequences if the given array is zero-plentiful else 0.
+
+function zeroPlentiful(arr){
+  console.log(arr);
+  return 0;
+}
