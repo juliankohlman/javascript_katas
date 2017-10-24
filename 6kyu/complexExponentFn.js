309bfa3f487087d5ef9f9ff0fff30542bbc35f4d
Julian Kohlman <juliankohlman@gmail.com>
2017-10-24 11:58:00 -0400

    Computing complex exponential function 6kyu solution

---
+++ 6kyu/complexExponentFn.js
@@ -0,0 +1,10 @@
+// Compute the exponential function at any given complex number, accurate to
+// at least 1 in 10^-12.
+
+function exp ([a, b]) {
+  return [Math.exp(a) * Math.cos(b), Math.exp(a) * Math.sin(b)];
+}
+
+exp([3,0])
+exp([3, Math.PI])
+exp([0, 9.42477796076938])
