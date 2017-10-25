91839aed9e8f6b82f92d6b798396f9bebf791c66
Julian Kohlman <juliankohlman@gmail.com>
2017-10-25 18:49:21 -0400

    Working String Array Revisal Solution 6kyu

--- 6kyu/stringArrRevisal.js
+++ 6kyu/stringArrRevisal.js
@@ -10,5 +10,14 @@
 // case. See test cases for more examples.
 
 function dup (s) {
-  // body...
+  return s.map(i => i.replace(/(.)\1+/g, m => m[0]));
 }
+
+dup(['ccooddddddewwwaaaaarrrrsssss', 'piccaninny', 'hubbubbubboo']) // ['codewars','picaniny','hubububo']);
+dup(['abracadabra', 'allottee', 'assessee']) // ['abracadabra','alote','asese']);
+dup(['kelless', 'keenness']) // ['keles','kenes']);
+dup(['Woolloomooloo', 'flooddoorroommoonlighters', 'chuchchi']) // ['Wolomolo','flodoromonlighters','chuchchi']);
+dup(['adanac', 'soonness', 'toolless', 'ppellee']) // ['adanac','sones','toles','pele']);
+dup(['callalloo', 'feelless', 'heelless']) // ['calalo','feles','heles']);
+dup(['putteellinen', 'keenness']) // ['putelinen','kenes']);
+dup(['kelless', 'voorraaddoosspullen', 'achcha']) // ['keles','voradospulen','achcha']);
