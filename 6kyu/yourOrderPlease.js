6aafb3baedc60fa2e126efd4ca491c9b6e3b65a0
Julian Kohlman <juliankohlman@gmail.com>
2017-10-26 20:51:41 -0400

    Add ES6 version of Your Order Please 6kyu

--- 6kyu/yourOrderPlease.js
+++ 6kyu/yourOrderPlease.js
@@ -13,5 +13,10 @@
 // [1] "Thi1s is2 3a T4est"
 
 function order (words) {
-  // ...
+  words.split(' ').sort((a,b) => a.match(/\d/) > b.match(/\d/)).join(' ');
 }
+
+// ES6 version
+const order = (words) => words.split(' ').sort((a,b)=>a.match(/\d/) > b.match(/\d/)).join(' ');
+
+order("is2 Thi1s T4est 3a")
