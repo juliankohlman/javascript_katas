3a37ce5d23f2d6e5dcad337b8715b58102a96a6e
Julian Kohlman <juliankohlman@gmail.com>
2017-10-30 09:24:11 -0400

    Solution for Sort the Odds 6kyu

--- 6kyu/sortTheOdd.js
+++ 6kyu/sortTheOdd.js
@@ -10,5 +10,10 @@
 // sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 
 function sortArray (array) {
-  // Return a sorted array.
+  let sortedOdds = array.filter(i => i % 2 !== 0).sort((a, b) => a - b);
+  return array.map(n => n % 2 === 1 ? sortedOdds.shift() : n);
 }
+
+sortArray([5, 3, 2, 8, 1, 4]) // [1, 3, 2, 8, 5, 4]
+sortArray([5, 3, 1, 8, 0]) // [1, 3, 5, 8, 0]
+sortArray([]) // []
