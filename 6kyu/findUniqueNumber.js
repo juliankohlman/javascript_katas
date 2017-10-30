c094d55180a077f260719ad68d41ffa4421aa83f
Julian Kohlman <juliankohlman@gmail.com>
2017-10-30 09:53:06 -0400

    Add solutions from codewars for comparison

---
+++ 6kyu/findUniqueNumber.js
@@ -0,0 +1,27 @@
+// There is an array with some numbers. All numbers are equal except for one.
+// Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+  let ascSort = arr.sort((a,b) => a - b);
+  return ascSort[0] === ascSort[1] ? ascSort.pop() : ascSort[0]
+}
+
+// CodeWars solutions
+// function findUniq(arr) {
+//   arr.sort((a,b)=>a-b);
+//   return arr[0]==arr[1]?arr.pop():arr[0]
+// }
+
+// function findUniq(arr) {
+//   return +arr.filter( (value) => { return arr.indexOf(value) == arr.lastIndexOf(value) } );
+// }
+
+findUniq([ 0, 1, 0 ]) // 1
+findUniq([ 1, 1, 1, 2, 1, 1 ]) // 2
+findUniq([ 3, 10, 3, 3, 3 ]) // 10
