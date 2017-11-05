178a50fc36a0d11acef5d8b551ee37451391f8e5
Julian Kohlman <juliankohlman@gmail.com>
2017-11-05 17:06:13 -0500

    Largest cross sum 6kyu

---
+++ 6kyu/largestCrossSum.js
@@ -0,0 +1,20 @@
+// Given a matrix, find the cross (row and column) with the largest sum of
+// elements. Return the sum.
+
+// [[1, 2, 3],
+//  [4, 5, 6],
+//  [7, 8, 9]]
+
+// largest cross is the last column, [3,6,9], with the last row, [7,8,9].
+
+// Sum of elements is 3 + 6 + 7 + 8 + 9 = 33
+
+// therefore largestCrossSum(matrix) = 33
+
+// NOTE: the shared element of the column and row should only be counted once.
+
+// The matrix may not be square. All elements will be positive integers.
+
+function largestCrossSum(matrix) {
+  // your code here
+}
