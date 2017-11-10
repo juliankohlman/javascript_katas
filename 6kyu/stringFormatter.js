51fc3a5cea0fb62128273122001499e8866a16ca
Julian Kohlman <juliankohlman@gmail.com>
2017-11-10 00:06:53 -0500

    Last String Formatter refactor

--- 6kyu/stringFormatter.js
+++ 6kyu/stringFormatter.js
@@ -15,14 +15,15 @@
 // ##filter The filter rule will always be applied last, this works the same as
 // the map rule but will be passed into the filter method.
 
-function StringFormatter (rules) {
+function StringFormatter(rules){
   const r = str => str.split('').map(i => rules.remove.includes(i) ? '' : i).join('');
   const m = str => str.split('').map(rules.map).join('');
   const f = str => str.split('').filter(rules.filter).join('');
-  return function (str) {
-    const len = Object.keys(rules).length;
+  return function(str) {
+    const len = Object.keys(rules).length
     if (len === 1) { return 'remove' in rules ? r(str) : 'map' in rules ? m(str) : f(str); }
-    if (len === 2) { return 'remove' in rules && 'map' in rules ? m(r(str)) : f(r(str)); }
+    if (len === 2 && 'remove' in rules && 'map' in rules) { return m(r(str)) }
+    if (len === 2 && 'remove' in rules && 'filter' in rules) { return f(r(str)) }
     return len === 3 ? f(m(r(str))) : f(m(str));
-  };
-}
+  }
+};
