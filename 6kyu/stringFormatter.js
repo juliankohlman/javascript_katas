beda03ac24e21eda39a147b03d2b3127c7d9a8a3
Julian Kohlman <juliankohlman@gmail.com>
2017-11-09 23:59:06 -0500

    Refactored String Formatter 6kyu

--- 6kyu/stringFormatter.js
+++ 6kyu/stringFormatter.js
@@ -15,44 +15,14 @@
 // ##filter The filter rule will always be applied last, this works the same as
 // the map rule but will be passed into the filter method.
 
-function StringFormatter(rules){
-//   console.log(Object.keys(rules).length)
-  console.log(Object.keys(rules).sort().reverse());
-//   console.log('remove' in rules)
-//   let rulesToApply = Object.keys(rules).length;
+function StringFormatter (rules) {
   const r = str => str.split('').map(i => rules.remove.includes(i) ? '' : i).join('');
   const m = str => str.split('').map(rules.map).join('');
   const f = str => str.split('').filter(rules.filter).join('');
-  return function(str) {
-    let len = Object.keys(rules).length
-
-    if (len === 3) { return f(m(r(str))) }
-    if (len === 1 && 'remove' in rules) { return r(str) }
-
-    if (len === 1 && 'map' in rules) { return m(str) }
-
-    if (len === 1 && 'filter' in rules) { return f(str) }
-
-    if (len === 2 && 'remove' in rules && 'map' in rules) { return m(r(str)) }
-    if (len === 2 && 'remove' in rules && 'filter' in rules) { return f(r(str)) }
-    if (len === 2 && 'filter' in rules && 'map' in rules) { return f(m(str)) }
-//     return str;
-  }
-
-  //   const r = str => str.split('').map(i => rules.remove.includes(i) ? '' : i).join('');
-  // const m = str => str.split('').map(i => i));
-  // const f = str => str.split('').filter(i => rules.filter(i)).join('');
-
-  // return function(str) {
-  //   if ('remove' in rules) {
-  //     str = r(str);
-  //   }
-  //   if ('map' in rules) {
-  //      str = m(str);
-  //   }
-  //   if ('filter' in rules) {
-  //     str = f(str);
-  //   }
-  //   return str;
-  // }
+  return function (str) {
+    const len = Object.keys(rules).length;
+    if (len === 1) { return 'remove' in rules ? r(str) : 'map' in rules ? m(str) : f(str); }
+    if (len === 2) { return 'remove' in rules && 'map' in rules ? m(r(str)) : f(r(str)); }
+    return len === 3 ? f(m(r(str))) : f(m(str));
+  };
 }
