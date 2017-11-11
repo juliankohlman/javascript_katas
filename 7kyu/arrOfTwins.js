3463570ced9d105aa08bd23965f789a82342c5a7
Julian Kohlman <juliankohlman@gmail.com>
2017-11-11 05:36:58 -0500

    Array of Twins 7kyu

---
+++ 7kyu/arrOfTwins.js
@@ -0,0 +1,22 @@
+// You are given an array of numbers. Check if the array contains of pairs, i.e
+// every number occurs exactly two times. If true, return true, otherwise false.
+// Your solution should not modify the input array.
+
+function twins(arr){
+  let res = arr.slice().sort((a,b) => a - b);
+  let allTwins = true;
+  for (let i = 0; i < res.length; i += 2) {
+    if (res[i] === res[i+1] && res[i] !== res[i + 2]) {
+      allTwins;
+    } else {
+      allTwins = false
+      break
+    }
+  }
+  return allTwins;
+}
+
+// CodeWars solution
+// function twins(myArray){
+//   return myArray.every(x => myArray.filter(y => x == y).length == 2)
+// }
