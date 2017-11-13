6a8eb214c179f8a585954e1270a6a6316573aaf2
Julian Kohlman <juliankohlman@gmail.com>
2017-11-13 11:11:46 -0500

    MaxMin Array 7kyu

---
+++ 7kyu/maxMin.js
@@ -0,0 +1,21 @@
+// In this Kata, you will be given an array of unique elements, and your task is
+// to rerrange the values so that the first max value is followed by the first
+// minimum, followed by second max value then second min value, etc.
+
+// For example:
+
+// solve([15,11,10,7,12]) = [15,7,12,10,11]
+
+// The first max is 15 and the first min is 7. The second max is 12 and the
+// second min is 10 and so on.
+
+function solve(arr){
+  let res = [];
+  let a = arr;
+  let sorted = a.sort((a,b)=> a < b)
+  while (sorted.length > 0) {
+    res.push(sorted.shift(),sorted.pop());
+    if (sorted.length === 1) { res.push(sorted.shift())}
+  }
+  return res
+};
