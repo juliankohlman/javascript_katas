da53a21e0f3c728c19a0e4fa66dac6932959124d
Julian Kohlman <juliankohlman@gmail.com>
2017-11-16 10:57:01 -0500

    peel pairs 7kyu

---
+++ 7kyu/peelPairs.js
@@ -0,0 +1,26 @@
+Given a string of numbers and a number n, peel away the pairs of digits that
+add up to n (starting from index 0). The numbers in the pair don't have to be
+adjacent to each other, but they should be paired with the closest digit to
+the right of them that sums to n.
+
+Return the new string with those pairs removed.
+
+peelPairs('14642', 3) => '464'
+peelPairs('732374', 6) => '77'
+peelPairs('245638363', 10) => '53363'
+
+function peelPairs(str, n) {
+//   console.log(str)
+//   let nums = str.split('').map(i=>+i).filter(i=>i>n)
+//   console.log(nums)
+//   if (nums.length > 0) return nums.join('')
+// //   return nums.filter(function(i,idx,col){
+// //     return !col.find((a,ind,arr) => i + a === n)
+// //   }).join('')
+  for (chr of str) {
+    console.log(chr);
+    console.log(str.includes(n - chr) && n - chr !== chr)
+  }
+}
+
+
