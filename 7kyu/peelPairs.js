f1868a000e7defa46eef3a6a4446c567de54fbda
Julian Kohlman <juliankohlman@gmail.com>
2017-11-16 16:29:08 -0500

    peel pairs recursive solution

--- 7kyu/peelPairs.js
+++ 7kyu/peelPairs.js
@@ -1,28 +1,22 @@
-Given a string of numbers and a number n, peel away the pairs of digits that
-add up to n (starting from index 0). The numbers in the pair don't have to be
-adjacent to each other, but they should be paired with the closest digit to
-the right of them that sums to n.
+// Given a string of numbers and a number n, peel away the pairs of digits that
+// add up to n (starting from index 0). The numbers in the pair don't have to be
+// adjacent to each other, but they should be paired with the closest digit to
+// the right of them that sums to n.
 
-Return the new string with those pairs removed.
-
-peelPairs('14642', 3) => '464'
-peelPairs('732374', 6) => '77'
-peelPairs('245638363', 10) => '53363'
+// Return the new string with those pairs removed.
 
 function peelPairs(str, n) {
-  let p = [];
-  for (chr of str) {
-    console.log(chr);
-//     console.log(str.includes(n - chr) && n - chr !== chr)
-    if (str.includes(n - chr)) {
-      console.log(str.indexOf(chr), str.lastIndexOf(n - chr))
-      if (str.indexOf(chr) !== str.lastIndexOf(n - chr)) {
-        p.push([str[str.indexOf(chr)],str[str.lastIndexOf(n - chr)]])
+  let p = str.split('').map(i => +i)
+  for (let i = 0; i < str.length; i += 1) {
+    for (let j = i + 1;  j < str.length; j += 1) {
+      if(p[i] + p[j] === n) {
+        return peelPairs(str.replace(p[i],'').replace(p[j],''), n);
       }
     }
   }
-//    && str.indexOf(chr) !== str.lastIndexOf(chr)
-  console.log(p)
+  return str;
 }
 
-
+peelPairs('14642', 3) // => '464'
+peelPairs('732374', 6) // => '77'
+peelPairs('245638363', 10) // => '53363'
