1fbfb075f76a43a7ca7baebbacd13ad7e542f868
Julian Kohlman <juliankohlman@gmail.com>
2017-11-29 23:04:15 -0500

    Sort by areas 7kyu

---
+++ 7kyu/sortRectCircle.js
@@ -0,0 +1,27 @@
+// In this kata you will be given an array of the dimensions of rectangles ( array with width and
+// length ) and circles ( radius - just a number ). Your task is to sort the objects by their area
+// in ascending order and return the sorted array of dimensions.
+
+// For example,
+
+// const array = [ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ];
+// // [ rectangle, circle, circle,rectangle ]
+
+// sortByArea(array) => [ [ 1.342, 3.212 ], 1.23, [ 4.23, 6.43 ], 3.444 ]
+
+function sortByArea(array) {
+  const areas = {};
+  const inputCopy = array.slice(0);
+  inputCopy.forEach((dimension) => {
+    if (Array.isArray(dimension)) {
+      areas[dimension] = dimension.reduce((a, b) => a * b);
+    } else {
+      areas[dimension] = Math.PI * (dimension ** 2);
+    }
+  });
+  return inputCopy.sort((a, b) => areas[a] - areas[b]);
+}
+
+sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]]);// [ [1.342, 3.212], 1.23, [4.23, 6.43], 3.444 ]
+sortByArea([[2, 5], 6]);// [ [2, 5], 6 ]
+sortByArea([]);// []
