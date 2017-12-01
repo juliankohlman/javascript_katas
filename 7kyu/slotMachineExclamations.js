8c22388244d7c64e90333ab2d414cb3879497bcd
Julian Kohlman <juliankohlman@gmail.com>
2017-11-30 20:07:27 -0500

    Slot Machine exclamation marks 7kyu

---
+++ 7kyu/slotMachineExclamations.js
@@ -0,0 +1,44 @@
+// Youareplayingasimpleslotmachinethatonlycontainsexclamationmarksandquestionmarks.Everytimetheslotmac
+// hineisstarted,astringof5lengthisobtained.Ifyou'reluckyenoughtogetaSpecialpermutation,you'llwinthebo
+// nus.Giveyouastrings,returnthehighestbonus.
+
+// Bouns list:
+
+//  Five-of-a-Kind:   ---- 1000
+//  "!!!!!" or "?????"
+
+//  Four-of-a-Kind:    ---- 800
+//  The string contains a "!!!!" or "????"
+//  "!!!!?","?!!!!","????!","!????"
+
+//  Full House:         ----500
+//  such as "!!!??" or "???!!"...
+
+//  Three-of-a-Kind:    ---- 300
+//  The string contains a "!!!" or "???"
+//  such as "!!!?!","!???!"...
+
+//  Two pair:           ---- 200
+//  The string contains two "!!" or "??"
+//  such as "??!!?","!!?!!"
+
+//  A Pair:             ---- 100
+//  The string contains a "!!" or "??"
+//  such as "?!??!","!!?!?"
+
+//  Others              ---- 0
+//  such as "?!?!?","!?!?!"
+
+// Examples
+
+// slot("!!!!!") === 1000
+// slot("!!!!?") === 800
+// slot("!!!??") === 500
+// slot("!!!?!") === 300
+// slot("!!?!!") === 200
+// slot("!!?!?") === 100
+// slot("!?!?!") === 0
+
+function slot(s){
+  //coding and coding....
+}
