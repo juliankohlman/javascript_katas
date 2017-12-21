8a98943498066657696342d7c159856b7e9284ae
Julian Kohlman <juliankohlman@gmail.com>
2017-12-21 11:27:41 -0500

    Refactor Next Palindromic number exercise

---
+++ lambdaCodeChallenges/nextPalindrome.js
@@ -0,0 +1,11 @@
+// Given a number, find the next smallest palindrome number larger than the number. If the number is
+// 125. the next palindrome is 131.
+
+function nextPalindrome(n) {
+  n += 1;
+  let palindrome = (n + '').split('').reverse().join('');
+  if (n == palindrome) return n;
+  return nextPalindrome(n);
+}
+
+nextPalindrome(125);
