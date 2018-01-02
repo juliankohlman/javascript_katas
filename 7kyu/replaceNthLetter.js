84e327f8f67e4f5d08f57b322458383501229ad8
Julian Kohlman <juliankohlman@gmail.com>
2018-01-02 10:30:06 -0500

    Every nth char solution

--- 7kyu/replaceNthLetter.js
+++ 7kyu/replaceNthLetter.js
@@ -16,18 +16,21 @@
 // The text and the chars will never be null.
 
 function replaceNth(text, n, oldValue, newValue) {
-  console.log(arguments);
-  let words = text.split('');
+  const words = text.split('');
+  if (n <= 0 || n > words.filter(i => i === oldValue).length) return text;
   let nth = 0;
-  let vCount = text.split('').filter(i => i === oldValue).length;
-  console.log(vCount);
-  if (n <= 0 || n > vCount) return text;
-  return words.map((l,idx) => {
+  return words.map((l) => {
     if (l === oldValue) {
-      nth += 1
-      return nth % n === 0 ? newValue : l
+      nth += 1;
+      return nth % n === 0 ? newValue : l;
     }
     return l;
   }).join('');
 }
 
+replaceNth('Vader said: No, I am your father!', 2, 'a', 'o'); // 'Vader soid: No, I am your fother!'
+replaceNth('Vader said: No, I am your father!', 4, 'a', 'o'); // 'Vader said: No, I am your fother!'
+replaceNth('Vader said: No, I am your father!', 6, 'a', 'o'); // 'Vader said: No, I am your father!'
+replaceNth('Vader said: No, I am your father!', 0, 'a', 'o'); // 'Vader said: No, I am your father!'
+replaceNth('Vader said: No, I am your father!', -2, 'a', 'o'); // 'Vader said: No, I am your father!'
+replaceNth('Vader said: No, I am your father!', 1, 'i', 'y'); // 'Vader sayd: No, I am your father!'
