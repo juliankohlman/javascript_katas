c4654d6d2362fee83e12758c51f65a16e589b9a7
Julian Kohlman <juliankohlman@gmail.com>
2018-01-03 11:52:25 -0500

    Refactor string char freq 6kyu

--- 6kyu/stringCharFreq.js
+++ 6kyu/stringCharFreq.js
@@ -10,26 +10,24 @@
 // solve('aaaa') = true -- if we remove one character, the remaining characters have same count.
 // solve('wwwf') = true -- if we remove f, the remaining letters have same count.
 
-function solve(s){
-console.log(s);
- let freq = {}
- for (let l of s) {
-   freq[l] ? freq[l] += 1 : freq[l] = 1;
- }
- console.log(freq);
- let values = Object.values(freq);
- console.log(values);
- let len = Object.values(freq).length;
- if (values.every((i) => i === 1)) return true;
+function solve(s) {
+  const freq = {};
+  s.split('').forEach(i => freq[i] ? freq[i] += 1 : freq[i] = 1);
+  const values = Object.values(freq);
+  const len = Object.values(freq).length;
+  if (values.every(i => i === 1)) return true;
+  if (values.lastIndexOf(1) === 0 && values.slice(1).every(i => i === values[1])) return true;
+  const chrTotal = Object.values(freq).reduce((a, b) => a + b);
+  return (chrTotal - 1) % len === 0;
+}
 
- console.log(len)
- console.log(values);
- console.log(values.sort((a,b) => a - b));
- console.log(values)
- if (values.lastIndexOf(1) === 0 && values.slice(1).every((i) => i === values[1])) return true;
-//  if (values.lastIndexOf(1) === 0) return true;
- let chrTotal = Object.values(freq).reduce((a,b) => a + b);
- console.log(chrTotal);
-
- return (chrTotal - 1) % len === 0 ? true : false;
-};
+console.log(solve('aaaa')); // true
+solve('abba'); // false
+solve('abbba'); // true
+solve('aabbcc'); // fals
+solve('aaaabb'); // fals
+solve('aabbccdd'); // true
+solve('aabcde'); //  true
+solve('abcde'); // true
+solve('aaabcde'); // false
+solve('abbccc'); // false;
