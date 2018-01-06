8ce90f6a8b9b1d81a2e9ec091d7f34c01477f020
Julian Kohlman <juliankohlman@gmail.com>
2018-01-05 19:15:41 -0500

    Reformat replace letters kata

--- 6kyu/replaceLetters.js
+++ 6kyu/replaceLetters.js
@@ -12,11 +12,13 @@
 
 // // It is preloaded:
 
-// const alphabet =
-// ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u
-// ','v','w','x','y','z'] const consonants =
-// ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']; const
-// vowels = ['a','e','i','o','u'];
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u
+','v','w','x','y','z']
+
+const consonants =
+['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+
+const vowels = ['a','e','i','o','u'];
 
 function replaceLetters(word) {
   // good luck
