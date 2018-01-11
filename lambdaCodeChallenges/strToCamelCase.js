9dda3b203507682dfd49068f18d704bb1b11b4a7
Julian Kohlman <juliankohlman@gmail.com>
2018-01-11 11:18:47 -0500

    String to camel case solution

---
+++ lambdaCodeChallenges/strToCamelCase.js
@@ -0,0 +1,12 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel
+// casing. The first word within the output should be capitalized only if the original word was
+// capitalized.
+
+// toCamelCase("the-stealth-warrior")
+// returns "theStealthWarrior"
+// toCamelCase("The_Stealth_Warrior")
+// returns "TheStealthWarrior"
+
+const toCamelCase = (str) => str.split(/[-_]/).map((w, i) => i !== 0 ? w[0].toUpperCase() + w.substr(1) : w).join('');
+
+console.log(toCamelCase('the-stealth-warrior'));
