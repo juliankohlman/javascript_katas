91d390b16cf80e4fce178a3565a0045b06ea18ba
Julian Kohlman <juliankohlman@gmail.com>
2018-02-12 10:29:28 -0500

    Add longest consecutive boilerplate for coding exercise

---
+++ lambdaCodeChallenges/longestConsecutive.js
@@ -0,0 +1,13 @@
+// You are given an array of strings called arr and an integer k. Your task is to return the longest
+// string consisting of k consecutive strings from the array.
+
+// Example:
+
+// longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+
+function longestConsecutive(arr, k) {
+
+}
