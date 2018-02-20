10721e35207831d562258915c44cdf1783768e41
Julian Kohlman <juliankohlman@gmail.com>
2018-02-20 11:02:43 -0500

    Boilerplate code for operators exercise

---
+++ lambdaCodeChallenges/operators.js
@@ -0,0 +1,15 @@
+// Implement three functions called multiply, divide, and modulo.
+// These functions should multiply, divide, or return the remainder of two arguments.
+// Now for the tricky part: you can only use + and - to implement these functions.
+
+function multiply(x, y) {
+
+}
+
+function divide(x, y) {
+
+}
+
+function modulo(x, y) {
+
+}
