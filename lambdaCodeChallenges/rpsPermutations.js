74ff5bf1633dd7f54c7c83850c961bce99e521ae
Julian Kohlman <juliankohlman@gmail.com>
2018-02-26 11:36:11 -0500

    Boilerplate code for rps exercise

---
+++ lambdaCodeChallenges/rpsPermutations.js
@@ -0,0 +1,26 @@
+// Write a function called rockPaperScissors that will take a number n, and output all those
+// possibilities.
+
+// Example:
+// rockPaperScissors(1) outputs [[rock],[paper],[scissors]];
+// rockPaperScissors(2) outputs [[rock, rock], [rock, paper], [rock, scissors],
+//  [paper, rock], [paper, paper], [paper, scissors], [scissors, rock],
+//  [scissors, paper], [scissors, scissors]];
+
+function rockPaperScissors(n) {
+  // const outputLength = Math.pow(n, 3);
+  // let rpsSet = new Set([ [rock],[paper],[scissors] ]);
+  // let result = [];
+  // for (let i = 0; i < outputLength; i++) {
+  //  result.push(rpsSet)
+  // }
+  let rpsArr = [ ['rock'], ['paper'], ['scissors'] ];
+  if (n === 1) return rpsArr;
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= rpsArr.length; j++) {
+      result.push([rpsArr[j],rpsArr[i]]);
+    }
+  }
+  return result;
+}
