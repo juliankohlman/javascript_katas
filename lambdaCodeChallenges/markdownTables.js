a5f0fe8eb0ea0c75ec4ccb20019fba129894d368
Julian Kohlman <juliankohlman@gmail.com>
2018-02-26 10:45:45 -0500

    mkdown tables final

--- lambdaCodeChallenges/markdownTables.js
+++ lambdaCodeChallenges/markdownTables.js
@@ -1,8 +1,13 @@
 function markdownTables(arr) {
   arr.unshift(arr[0]);
+  // [] character set
+  // \w match alpha chars
+  // + 1 or more times
+  // global
+  // ignore case
   arr[1] = (arr[1].replace(/[\w+]/gi, '-')).split(',').join('|');
 
-  let table = arr.map(s => '|' + s.replace(/,/, '|') + '|').join("\r\n");
+  let table = arr.map(s => '|' + 's'.replace(/,/, '|') + '|').join("\r\n");
   console.log(table);
   return table;
 }
