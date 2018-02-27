7906c06f51763633049ca486ac36d46fc7f0cea8
Julian Kohlman <juliankohlman@gmail.com>
2018-02-27 11:10:24 -0500

    Merged Objects coding exercise

---
+++ lambdaCodeChallenges/mergedObjects.js
@@ -0,0 +1,25 @@
+// Write a function that given an array of objects will return a single 'merged' object, where
+// duplicate keys have the last value given.
+
+// For example:
+// [
+// {1: '1', 2: '2', 3: '3'}
+// {3: '4', 4: '4', 5: '6'}
+// {5: '5', 6: '6', 7: '7'}
+// ];
+// Should return:
+
+// {1: '1', 2: '2': 3: '4', 4: '4', 5: '5', 6: '6', 7: '7'}
+// where the value of 5 is '5' because '5' it was the last given value for that key.
+
+function mergeObjects(arr) {
+  let mergedObj = {};
+  arr.forEach(obj => Object.assign(mergedObj, obj));
+  return mergedObj;
+}
+
+mergeObjects([
+{1: '1', 2: '2', 3: '3'},
+{3: '4', 4: '4', 5: '6'},
+{5: '5', 6: '6', 7: '7'}
+]);
