765631dc016c002da37f1b2c7b4c600d71598b1f
Julian Kohlman <juliankohlman@gmail.com>
2018-03-01 11:39:58 -0500

    Quick sort coding exercise

---
+++ lambdaCodeChallenges/quickSort.js
@@ -0,0 +1,20 @@
+// Implement the quick sort sorting algorithm. Assume the input is an array of integers.
+
+// https://en.wikipedia.org/wiki/Quicksort
+
+// https://www.khanacademy.org/computing/computer-science/algorithms#quick-sort
+
+function quickSort(nums) {
+  if (nums.length < 2) return nums;
+
+  const pivotValue = nums[Math.round(nums.length / 2)];
+  const pivots = nums.filter(n => n == pivotValue);
+
+  // DRY
+  const belowPivot = nums.filter(x => x < pivotValue).sort((a,b) => a - b);
+  const abovePivot = nums.filter(x => x > pivotValue).sort((a,b) => a - b);
+
+  return belowPivot.concat(pivots,abovePivot);
+}
+
+quickSort([11,3,7,2,5,84,22,1])
