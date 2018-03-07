e0a202bf5c5bc77eddede72b2111b1998a0325aa
Julian Kohlman <juliankohlman@gmail.com>
2018-03-07 11:27:40 -0500

    slight refactor and added comments for contiguous array sum exercise

---
+++ lambdaCodeChallenges/contiguousArrSum.js
@@ -0,0 +1,24 @@
+// Given an array of numbers, find the greatest contiguous sum of numbers.
+
+// example 1: sumArray([1, 2, 3]); // => 6
+// example 2: sumArray([4, -1, 5]); // => 8
+// example 3: sumArray([10, -11, 11]); // 11
+
+function sumArray(arr) {
+  let currentMaxSum = 0;
+  let endingMax = 0;
+
+  arr.forEach(i => {
+    endingMax += i; // -2, -3 same situation...4 is reached
+    if (endingMax < 0) { // 4 not less than zero skip to else if condition
+      endingMax = 0; // 0
+    } else if (currentMaxSum < endingMax) { // 0 < 0:  0 < 4 yes
+      currentMaxSum = endingMax; // 0 = 0 0 = 4 now ending max and currentMax are equal to 4 with endingMax taking on value of next array item
+    }
+  });
+  return currentMaxSum;
+}
+
+sumArray([-2, -3, 4, -1, -2, 1, 5, -3]);
+
+sumArray([-2, -3, 4, -1, -2, 1, 5, -3]);
