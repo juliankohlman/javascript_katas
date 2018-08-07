8c8c6c2bcb2f1510993ba2b0e1ad75fd322f67c2
Julian Kohlman <juliankohlman@gmail.com>
2018-08-07 11:17:49 -0400

    passing all but two tests  noted under test examples

--- 6kyu/transcriptionFactors.js
+++ 6kyu/transcriptionFactors.js
@@ -1,24 +1,3 @@
-/*
-The DNA is a long sequence of either cytosine(C), guanine(G), adenine(A), or thymine(T) and their order is crucial for the transmission of genetic information.The reading of this sequence doesn't always happen from the start to the end, but there are some regions that need to be repeated. To do so, the organisms developed a system composed by “transcription factors”. A transcription factor is a molecule that binds to specific DNA sequence (binding sites), thereby controlling the number of repetitions of that sequence.
-
-Given these group of transcription factors and relative binding sites:
-
-ATF6: "TGACGT"
-CREB: "TGACGCA"
-cMyc: "CACGTG"
-Gata1: "GATT"
-AhR: "TGCGTG"
-Write a function transFactors that takes the argument seq and returns the transcription factor(s) that bind and the binding position(s) in the sequence(starting from 1).The output should be an object containing an array of the name(s) of the transcription factor(s) and an array of the binding position(s).
-
-Please note that some transcription factors could have overlapping binding sites with others(or with themselves).
-
-For example:
-
-seq = "ATGGCTGACGTCGTCATGGCGCCCCGATTGAACGATTCCTCCTCCT"
-returns: { ATF6: [6], Gata1: [26, 34] }
-In case the sequence is empty, too short, or doesn't contain a match, the function should return and empty object {}.
-*/
-
 function transFactors(seq) {
 	const factors = {
 		ATF6: /TGACGT/g,
@@ -37,8 +16,25 @@ function transFactors(seq) {
 	for (let i = 0; i < vals.length; i += 1) {
 		while ((match = vals[i].exec(seq)) !== null) {
 			//       console.log("match found at " + match.index, match);
-			trueFactors[codes[i]] = [match.index + 1];
+			if (!trueFactors[codes[i]]) {
+				trueFactors[codes[i]] = [match.index + 1];
+			} else {
+				trueFactors[codes[i]].push(match.index + 1);
+			}
 		}
 	}
-	console.log(trueFactors);
+	//   console.log(trueFactors)
+	return trueFactors;
 }
+
+/* Test examples
+Test.assertSimilar(transFactors(""), {});
+Test.assertSimilar(transFactors("ACGT"), {});
+Test.assertSimilar(transFactors("GATT"), { Gata1: [1] });
+Test.assertSimilar(transFactors("TGACGTGACGT"), { ATF6: [1, 6] }); => FAILING TEST
+Test.assertSimilar(transFactors("TGCGTGCGTG"), { AhR: [1, 5] }); = FAILING TEST
+Test.assertSimilar(transFactors("CACGTGACGT"), { ATF6: [5], cMyc: [1] });
+Test.assertSimilar(transFactors("ATGGCCGTCATGGCGCCCCGAACCCTCCTCCTGCTACTCTCGGGGGCCCTGGCCCTGACCCAGACCTGGGCGGGCTCCCACTCCATGAGGTATTTCTTCACATCCGTGTCCCGGCCCGGCCGCGGGGAGCCCCGCTTCATCGCCGTGGGCTACGTGGACGACACGCAGTTCGTGCGGTTCGACAGCGACGCCGCGAGCCAGAGGATGGAGCCGCGGGCGCCGTGGATAGAGCAGGAGGGGCCGGAGTATTGGGACCAGGAGACACGGAATGTGAAGGCCC"), {});
+Test.assertSimilar(transFactors("GATTCGGCGGACATGGCGGCTCAGATCACCAAGCGCAAGTGGGAGGCGGCCCATGAGGCGGAGCAGTTGAGAGCCTACCTGGATGGCACGTCGTGGAGTGGCTCCGCAGATACCTGGAGAACGGGAAGGAGACGCTGCAGCGCACGGACCCCCCCAAGACACATATGACCCACCACCCCATCTCTGACCATGAGGCCACCCTGAGGTGCTGGGCCCTGGGCTTCTACCCTGCGGAGATCACACTGACCTGGCAGCGGGATGGGGAGGACCAGACCCAGGACACGGAGCTCGTGGAGACCAGGCCTGCAGGGGATGGAACCTTCCAGAAGTGGGCGGCTGTGGTGGTGCCTTCTGGAGAGGAGCAGAGATACACCTGCCATGTGCAGCATGAGGGTCTGCCCAAGCCCCTCACCCTGAGATGGGAGCTGTCTTCCCAGCCCACCATCCCCATCGTGGGCATCATTGCTGGCCTGGTTCTCCTTGGAGCTGTGATCACTGGAGCTGTGGTCGCTGCCGTGATGTGGAGGAGGAAGAGCTCAGATAGAAAAGGAGGGAGTTACACTCAGGCTGCAAGCAGTGACAGTGCCCAGGGCTCTGATGTGTCCCTCACAGCTTGTAAAGTGTGACACGTG"), { cMyc: [627], Gata1: [1] });
+Test.assertSimilar(transFactors("GATTCGGCGGACATGGCGGCTCAGATCACCAAGCGCAAGTGGGAGGCGGCCCATGAGGCGGAGCAGTTGAGAGCCTACCTGGATGGCACGTGCGTGGAGTGGCTCCGCAGATACCTGGAGAACGGGAAGGAGACGCTGCAGCGCACGGACCCCCCCAAGACACATATGACCCACCACCCCATCTCTGACCATGAGGCCACCCTGAGGTGCTGGGCCCTGGGCTTCTACCCTGCGGAGATCACACTGACCTGGCAGCGGGATGGGGAGGACCAGACCCAGGACACGGAGCTCGTGGAGACCAGGCCTGCAGGGGATGGAACCTTCCAGAAGTGGGCGGCTGTGGTGGTGCCTTCTGGAGAGGAGCAGAGATACACCTGCCATGTGCAGCATGAGGGTCTGCCCAAGCCCCTCACCCTGAGATGGGAGCTGTCTTCCCAGCCCACCATCCCCATCGTGGGCATCATTGCTGGCCTGGTTCTCCTTGGAGCTGTGATCACTGGAGCTGTGGTCGCTGCCGTGATGTGGAGGAGGAAGAGCTCAGATAGAAAAGGAGGGAGTTACACTCAGGCTGCAAGCAGTGACAGTGCCCAGGGCTCTGATGTGTCCCTCACAGCTTGTAAAGTGTGACACGTG"), { cMyc: [87, 628], Gata1: [1], AhR: [91] });
+*/
