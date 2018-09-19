34affc4cacf110872e69b327a5e9648d06b4a40f
Julian Kohlman <juliankohlman@gmail.com>
2018-09-19 19:30:51 -0400

    roman numeralize with object holding data

--- lambdaCodeChallenges/romanNumeralize.js
+++ lambdaCodeChallenges/romanNumeralize.js
@@ -1,4 +1,20 @@
 function romanNumeralize(number) {
+	const data = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1
+	};
+
 	const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 	const numerals = [
 		'M',
@@ -16,14 +32,26 @@ function romanNumeralize(number) {
 		'I'
 	];
 
-	for (let i = 0; i < decimals.length; i++) {
+	// for (let i = 0; i < decimals.length; i++) {
+	// 	if (number < 1) return '';
+	// 	if (number >= decimals[i]) {
+	// 		// M + romanNumeralize(1990 - 1000)
+	// 		// MCM + romanNumeralize(990 - 900)...
+	// 		return numerals[i] + romanNumeralize(number - decimals[i]);
+	// 	}
+	// }
+
+	for (let i = 0; i < Object.keys(data).length; i++) {
 		if (number < 1) return '';
-		if (number >= decimals[i]) {
+		if (number >= Object.values(data)[i]) {
 			// M + romanNumeralize(1990 - 1000)
 			// MCM + romanNumeralize(990 - 900)...
-			return numerals[i] + romanNumeralize(number - decimals[i]);
+			return (
+				Object.keys(data)[i] + romanNumeralize(number - Object.values(data)[i])
+			);
 		}
 	}
 }
 
-romanNumeralize(1990);
+console.log(romanNumeralize(1990));
+console.log(romanNumeralize(1973));
