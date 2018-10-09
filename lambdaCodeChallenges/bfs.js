8f5d271dbd07889a52ce0bc85c84bd4adaaa68f7
Julian Kohlman <juliankohlman@gmail.com>
2018-10-09 18:57:57 -0400

    add notes for bfs

--- lambdaCodeChallenges/bfs.js
+++ lambdaCodeChallenges/bfs.js
@@ -27,3 +27,19 @@ breadthFirstSearch(deep, 16); will return true.
 breadthFirstSearch(deep, 'c'); will return true.
 */
 function breadthFirstSearch(obj, searchTerm) {}
+
+/*
+function breadthFirstSearch(obj, searchTerm) {
+  // console.log(obj);
+  let q = Object.values(obj);
+  // console.log(q);
+  while (q.length > 0) {
+    let value = q.shift();
+    // console.log(value);
+    if (value === searchTerm) return true;
+    if (typeof value === 'object' ) q = q.concat(Object.values(value));
+    // console.log(q);
+  }
+  return false;
+}
+*/
