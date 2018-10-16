d150253369dadccd0ee25945d5dd6680c6049885
Julian Kohlman <juliankohlman@gmail.com>
2018-10-16 09:38:03 -0400

    Add in-place algo version of digits sum

--- lambdaCodeChallenges/sumOfDigits.js
+++ lambdaCodeChallenges/sumOfDigits.js
@@ -8,15 +8,30 @@ console.log(sumOfDigits(496)); // <--- 19
 sumOfDigits(23) returns 5 because 2 + 3 = 5
 sumOfDigits(496) returns 19 because 4 + 9 + 6 = 19
 */
+
+// Version 1 //
+// --------- //
 function sumOfDigits(num) {
-	let sum = num
-		.toString()
-		.split('')
-		.reduce((acc, curr) => {
-			return +acc + +curr;
-		});
+	let numString = num.toString();
+	let sum = 0;
+	for (let i = 0; i < numString.length; i++) {
+		sum += +numString[i];
+	}
 	return sum;
 }
+
+// Version 2 //
+// --------- //
+
+// function sumOfDigits(num) {
+// 	let sum = num
+// 		.toString()
+// 		.split('')
+// 		.reduce((acc, curr) => {
+// 			return +acc + +curr;
+// 		});
+// 	return sum;
+// }
 
 console.log(sumOfDigits(23)); // <--- 5
 console.log(sumOfDigits(496)); // <--- 19
