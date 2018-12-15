7ba419c2c1f43638fb8158ef35a9fc259250c7a4
Julian Kohlman <juliankohlman@gmail.com>
2018-12-14 20:06:25 -0500

    Add comments for twinprimes and contiguous sum exercises

--- lambdaCodeChallenges/twinPrimes.js
+++ lambdaCodeChallenges/twinPrimes.js
@@ -7,13 +7,14 @@
 // However, 7 - 2 = 5, which is a prime, so it returns true. 23 is a prime, but 23 + 2 is 25, which
 // is not a prime.  23 - 2 is 21, which isn't a prime either, so 23 is not a twin prime.
 
+// * Add comments breaking down solution further (including use of helper function)
 const isPrime = num => {
-  for(let i = 2, sq = Math.sqrt(num); i <= sq; i++)
-    if (num % i === 0) return false;
-    return num >= 2;
+	for (let i = 2, sq = Math.sqrt(num); i <= sq; i++)
+		if (num % i === 0) return false;
+	return num >= 2;
 };
 
 function isTwinPrime(n) {
-  if (!isPrime(n)) return isPrime(n);
-  return (isPrime(n - 2) || isPrime(n + 2));
+	if (!isPrime(n)) return isPrime(n);
+	return isPrime(n - 2) || isPrime(n + 2);
 }
