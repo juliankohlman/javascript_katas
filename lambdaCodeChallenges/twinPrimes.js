da5bda4bd155922700ff710fe9e1ef35170e3e44
Julian Kohlman <juliankohlman@gmail.com>
2019-01-16 21:13:23 -0500

    Refactors TwinPrime function to call return false when not prime

--- lambdaCodeChallenges/twinPrimes.js
+++ lambdaCodeChallenges/twinPrimes.js
@@ -6,15 +6,23 @@
 // does not need checking, so it returns false. 7 is a prime, but 7 + 2 = 9, which is not a prime.
 // However, 7 - 2 = 5, which is a prime, so it returns true. 23 is a prime, but 23 + 2 is 25, which
 // is not a prime.  23 - 2 is 21, which isn't a prime either, so 23 is not a twin prime.
+/* Explanation for why Math.sqrt is used => https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
+ */
 
 // * Add comments breaking down solution further (including use of helper function)
 const isPrime = num => {
+	// using Math.sqrt deals with looking at factors of a number to determine
+	// the number of factors we need to check (will be <= sq)
 	for (let i = 2, sq = Math.sqrt(num); i <= sq; i++)
 		if (num % i === 0) return false;
 	return num >= 2;
 };
 
-function isTwinPrime(n) {
-	if (!isPrime(n)) return isPrime(n);
-	return isPrime(n - 2) || isPrime(n + 2);
-}
+const isTwinPrime = n => {
+	// if the number is prime check for a twinPrime condition
+	if (isPrime(n)) return isPrime(n - 2) || isPrime(n + 2);
+	// if the number is not a prime return true
+	return false;
+};
+
+console.log(isTwinPrime(4));
