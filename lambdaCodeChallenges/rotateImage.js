5de3a0e18cdaf59c4364e7e006f3f55a6c2c77ca
Julian Kohlman <juliankohlman@gmail.com>
2019-01-22 20:11:34 -0500

    Add second version of imageRotation exercise

--- lambdaCodeChallenges/rotateImage.js
+++ lambdaCodeChallenges/rotateImage.js
@@ -25,3 +25,36 @@ should return
   [ 1, 2, 3, 4, 5 ],
   [ 1, 2, 3, 4, 5 ] ]
  */
+
+// es6 version
+function rotateImage(a) {
+	const len = a.length - 1;
+	// clockwise a[len - j][i]
+	const rotated = a.map((row, i) => row.map((val, j) => a[j][len - i]));
+	a.length = 0;
+	a.push(...rotated);
+	return a;
+}
+
+// function rotateImage(arr) {
+// 	const len = arr.length;
+// 	for (let i = 0; i < len / 2; i++) {
+// 		for (let j = i; j < len - i - 1; j++) {
+// 			let bucket = arr[i][j];
+// 			arr[i][j] = arr[j][len - i - 1];
+// 			arr[j][len - i - 1] = arr[len - i - 1][len - j - 1];
+// 			arr[len - i - 1][len - j - 1] = arr[len - j - 1][i];
+// 			arr[len - j - 1][i] = bucket;
+// 		}
+// 	}
+// 	return arr;
+// }
+console.log(
+	rotateImage([
+		[1, 1, 5, 9, 9],
+		[2, 2, 6, 0, 0],
+		[3, 3, 7, 1, 1],
+		[4, 4, 8, 2, 2],
+		[5, 5, 9, 3, 3]
+	])
+);
