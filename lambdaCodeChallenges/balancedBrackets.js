952dbaa1bc93953080c92fe5dd880253b46b9a6b
Julian Kohlman <juliankohlman@gmail.com>
2019-01-30 22:02:48 -0500

    Additional comments on balanced bracket exercise

--- lambdaCodeChallenges/balancedBrackets.js
+++ lambdaCodeChallenges/balancedBrackets.js
@@ -19,15 +19,20 @@
 // balancedBrackets(' const obj  = { x: someFunction() }'); // true
 
 function balancedBrackets(str) {
-  let s = str.replace(/[^\(\)|\[\]|\{\}]/g,'')
-  let a = s, b;
-  do {
-    b = a; // initialize b to a
-    a = a.replace(/\(\)|\[\]|\{\}/g, '');
-    console.log('a', a) // if str was balanced it becomes ''
-    console.log(b)      // if str was not balanced some chars will remain
-  } while (a != b); // this condition will be met b/c we called replace on a
-  return !a; // !'' === true !'(' === false
+	let s = str.replace(/[^\(\)|\[\]|\{\}]/g, ''); // match any chars NOT in this set
+	let compareStr = s,
+		temp;
+	do {
+		temp = compareStr; // initialize b to a
+		console.log('a', compareStr);
+		compareStr = compareStr.replace(/\(\)|\[\]|\{\}/g, ''); // replacing balanced brackets from the inside out
+		console.log('a', compareStr); // if str was balanced it becomes ''
+		console.log(temp); // if str was not balanced some chars will remain
+	} while (compareStr != temp); // this condition will be met b/c we called replace on a
+	return !compareStr; // !'' === true !'(' === false
 }
+
+balancedBrackets('[({})]'); // true
+balancedBrackets('[]({}'); // false
 
 balancedBrackets('[]({}');
