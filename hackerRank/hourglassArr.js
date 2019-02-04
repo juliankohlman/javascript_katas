f115394d3dd98c0b04b680dff0193785c8349de3
Julian Kohlman <juliankohlman@gmail.com>
2019-02-04 11:46:38 -0500

    Minor tweak to result variable processing on hourglass solution

--- hackerRank/hourglassArr.js
+++ hackerRank/hourglassArr.js
@@ -29,3 +29,26 @@ function hourglassSum(arr) {
 }
 
 console.log(hourglassSum(arr2d));
+
+// Hackerrank all-in-one result processing version
+/*
+function hourglassSum(arr) {
+	let a, b, c, d, e, f, g;
+	let hourglasses = [];
+	for (let row = 0; row < 4; row++) {
+		for (let col = 0; col < 4; col++) {
+			a = arr[row][col]; // top left
+			b = arr[row][col + 1]; // top middle
+			c = arr[row][col + 2]; // top right
+			d = arr[row + 1][col + 1]; // center
+			e = arr[row + 2][col]; // bottom left
+			f = arr[row + 2][col + 1]; // bottom middle
+			g = arr[row + 2][col + 2]; // bottom right
+			hourglasses.push([a, b, c, d, e, f, g]);
+		}
+	}
+	return Math.max(
+		...hourglasses.map(nums => nums.reduce((current, next) => current + next))
+	);
+}
+*/
