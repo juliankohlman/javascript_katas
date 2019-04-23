90721fdbf0824916266bb72c78a1e8920dddff54
Julian Kohlman <juliankohlman@gmail.com>
2019-04-23 17:11:09 -0400

    Basic solution to rotleft hackerrank problem

---
+++ hackerRank/leftRotation.js
@@ -0,0 +1,18 @@
+// rotLeft
+// accepts and array (a) and an amount of rotations
+/*
+5 4
+[1,2,3,4,5]
+outputs [5,1,2,3,4]
+
+*/
+function rotLeft(a, d) {
+	while (d > 0) {
+		let rt = a.shift();
+		d--;
+		a.push(rt);
+	}
+	return a;
+}
+
+console.log(rotLeft([1, 2, 3, 4, 5], 4));
