511ddbc5b0aeecf3cb0aee906ec3ec662d76c014
Julian Kohlman <juliankohlman@gmail.com>
2019-04-24 17:33:21 -0400

    Initial solution to nyChaos exercise

--- hackerRank/nyChaos.js
+++ hackerRank/nyChaos.js
@@ -4,8 +4,24 @@ function minimumBribes(q) {
 	// returns integer representing # of bribes that where needed to get queue to its current state.
 	//
 	// create initial ordered queue
-
-	return q;
+	// [1, 2, 3, 4, 5]
+	// [2, 1, 5, 3, 4]
+	let initialQ = q.slice().sort();
+	let bribes = 0;
+	let tooChaotic = null;
+	q.forEach(person => {
+		if (initialQ.indexOf(person) - q.indexOf(person) > 2) {
+			tooChaotic = true;
+		} else if (
+			q.indexOf(person) <= initialQ.indexOf(person) &&
+			q.indexOf(person) - initialQ.indexOf(person) <= 2
+		) {
+			bribes += initialQ.indexOf(person) - q.indexOf(person);
+		}
+	});
+	return tooChaotic ? 'Too Chaotic' : bribes;
+	// return bribes;
 }
 
 console.log(minimumBribes([2, 1, 5, 3, 4]));
+console.log(minimumBribes([2, 5, 1, 3, 4]));
