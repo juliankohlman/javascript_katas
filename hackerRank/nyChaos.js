b1f46c975eed099561331be6ba983169072f62a3
Julian Kohlman <juliankohlman@gmail.com>
2019-04-24 16:54:44 -0400

    Initial setup for min bribe exercise

---
+++ hackerRank/nyChaos.js
@@ -0,0 +1,11 @@
+// minimum bribes
+
+function minimumBribes(q) {
+	// returns integer representing # of bribes that where needed to get queue to its current state.
+	//
+	// create initial ordered queue
+
+	return q;
+}
+
+console.log(minimumBribes([2, 1, 5, 3, 4]));
