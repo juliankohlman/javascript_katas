e4e7f0cefdd9fe6995473536810adf56889d6859
Julian Kohlman <juliankohlman@gmail.com>
2019-06-17 13:19:00 -0400

    Leetcode add two numbers boilerplate

---
+++ leetcode/addTwoNumbers.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * https://leetcode.com/problems/add-two-numbers/
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+/*
+ListNode {
+  val: 2,
+  next: ListNode { val: 4, next: ListNode { val: 3, next: null } } }
+*/
+var addTwoNumbers = function(l1, l2) {
+	// user recursion
+};
