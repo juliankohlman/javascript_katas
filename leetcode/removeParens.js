3b672ee1e81f8c96b81d8ae2c0306767dbc9df22
Julian Kohlman <juliankohlman@gmail.com>
2019-06-30 17:52:42 -0400

    Adds examples and testcases for outer parens exercise

--- leetcode/removeParens.js
+++ leetcode/removeParens.js
@@ -6,4 +6,38 @@ A valid parentheses string S is primitive if it is nonempty, and there does not 
 Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
 
 Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
  */
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const removeOuterParentheses = S => {};
