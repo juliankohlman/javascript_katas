62c930b7186cf5b0f1f512e40ca6bb31f35fdea1
Julian Kohlman <juliankohlman@gmail.com>
2019-09-14 21:21:07 -0400

    Adds hash to count common elements

--- 6kyu/commonArrayElements.js
+++ 6kyu/commonArrayElements.js
@@ -8,15 +8,21 @@ common([1,2,2,3],[5,3,2,2],[7,3,2,2]) = 7 because 2,2 & 3 are common in the 3 ar
 Array lengths in random tests run from 5000 to 10000 elements.
 
 More examples in the test cases.
-
 */
 
-// ! Work in progress
 function common(a, b, c) {
-	a.forEach(i => {
-		// b.includes(i) && c.include(i)
+	// keys count === number of arrays (in this case 3) its common
+	// Todo figure out how to handle duplicate common elements
+	let arrayCounts = {};
+	let allItems = a.concat(b, c);
+	allItems.forEach(element => {
+		arrayCounts[element]
+			? (arrayCounts[element] += 1)
+			: (arrayCounts[element] = 1);
 	});
-	// create sum variable
-	// create 2 hashes
-	//
+	console.log(arrayCounts);
+	console.log(allItems);
 }
+
+common([1, 2, 3], [5, 3, 2], [7, 3, 2]); // 5 because 2 & 3 are common in all 3 arrays
+common([1, 2, 2, 3], [5, 3, 2, 2], [7, 3, 2, 2]); // 7 because 2,2 & 3 are common in the 3 arrays
