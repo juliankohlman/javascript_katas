c52611e76912b15eeaedd2b3502967459591ed7e
Julian Kohlman <juliankohlman@gmail.com>
2019-09-26 20:18:12 -0400

    list intersection pseudocode

--- leetcode/Easy/getIntersection.js
+++ leetcode/Easy/getIntersection.js
@@ -5,6 +5,9 @@
  *     this.val = val;
  *     this.next = null;
  * }
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
  */
 
 /**
@@ -12,4 +15,7 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-const getIntersectionNode = function(headA, headB) {};
+const getIntersectionNode = function(headA, headB) {
+  // solve it on paper first
+  // absorb the problem and be aware of edge cases and time complexity
+};
