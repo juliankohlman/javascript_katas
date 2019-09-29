87fc5255f605b4469b646a7c9f08be24787417b7
Julian Kohlman <juliankohlman@gmail.com>
2019-09-28 20:12:18 -0400

    remove linked list elements

---
+++ leetcode/Easy/removeLinkedElement.js
@@ -0,0 +1,23 @@
+/**
+ * Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  // let currentNode = head
+  // while(currentNode.next) {
+  // }
+};
