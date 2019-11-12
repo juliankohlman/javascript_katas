db07bc27e727846748b413653c9050431c260f0b
Julian Kohlman <juliankohlman@gmail.com>
2019-11-11 19:07:40 -0500

    binary search variant rotation point breakdown

---
+++ interviewCake/findRotationPoint.js
@@ -0,0 +1,21 @@
+function findRotationPoint(words) {
+  //* pseudocode here
+  return false;
+}
+// Tests
+let smallArray = ["cape", "cake"]; // 1
+let medArray = ["grape", "orange", "plum", "radish", "apple"]; // 4
+let lgArray = [
+  "ptolemaic",
+  "retrograde",
+  "supplant",
+  "undulate",
+  "xenoepist",
+  "asymptote",
+  "babka",
+  "banoffee",
+  "engender",
+  "karpatka",
+  "othellolagkage"
+]; // 5
+let words = ["k", "v", "a", "b", "c", "d", "e", "g", "i"]; // 2
--- lambdaCodeChallenges/balancedBrackets.js
+++ lambdaCodeChallenges/balancedBrackets.js
@@ -19,20 +19,20 @@
 // balancedBrackets(' const obj  = { x: someFunction() }'); // true
 
 function balancedBrackets(str) {
-	let s = str.replace(/[^\(\)|\[\]|\{\}]/g, ''); // match any chars NOT in this set
-	let compareStr = s,
-		temp;
-	do {
-		temp = compareStr; // initialize b to a
-		console.log('a', compareStr);
-		compareStr = compareStr.replace(/\(\)|\[\]|\{\}/g, ''); // replacing balanced brackets from the inside out
-		console.log('a', compareStr); // if str was balanced it becomes ''
-		console.log(temp); // if str was not balanced some chars will remain
-	} while (compareStr != temp); // this condition will be met b/c we called replace on a
-	return !compareStr; // !'' === true !'(' === false
+  let s = str.replace(/[^\(\)|\[\]|\{\}]/g, ""); // match any chars NOT in this set
+  let compareStr = s,
+    temp;
+  do {
+    temp = compareStr; // initialize b to a
+    console.log("a", compareStr);
+    compareStr = compareStr.replace(/\(\)|\[\]|\{\}/g, ""); // replacing balanced brackets from the inside out
+    console.log("a", compareStr); // if str was balanced it becomes ''
+    console.log(temp); // if str was not balanced some chars will remain
+  } while (compareStr != temp); // this condition will be met b/c we called replace on a
+  return !compareStr; // !'' === true !'(' === false
 }
 
-balancedBrackets('[({})]'); // true
-balancedBrackets('[]({}'); // false
+balancedBrackets("[({})]"); // true
+balancedBrackets("[]({}"); // false
 
-balancedBrackets('[]({}');
+balancedBrackets("[]({}");
