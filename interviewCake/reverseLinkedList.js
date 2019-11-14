e018db413f32e93bcf3429d6ae347f0423839852
Julian Kohlman <juliankohlman@gmail.com>
2019-11-13 19:51:03 -0500

    reverse linked list debugging

--- interviewCake/reverseLinkedList.js
+++ interviewCake/reverseLinkedList.js
@@ -15,22 +15,29 @@ class LinkedListNode {
   }
 }
 
+//* Recursive implementation
+// reverseRecursiveList() {
+
+// }
+
 // ! Run through this by hand and write out the steps on whiteboard
+// ? Bonus: This in-place ↴ reversal destroys the input linked list. What if we wanted to keep a copy of the original linked list? Write a function for reversing a linked list out-of-place.
+
 function reverseList(headOfList) {
   // need to point each nodes next pointer to the previous node in place
   // create copy of head node
   let currentNode = headOfList;
   // create runner node prev
   let prevNode = null;
-  // create runner node next
+  // create temp variable to hold next pointer
   let nextNode = null;
 
   // loop over list until we reach it's end
   while (currentNode) {
-    // copy pointer to next BEFORE we overwrite it
+    // copy pointer to next BEFORE we overwrite it (saving next pointer)
     nextNode = currentNode.next;
 
-    // reverse the next pointer of current node
+    // reverse the next pointer of current node (pointing current node to previous node)
     currentNode.next = prevNode;
 
     // move forward in list
@@ -45,10 +52,11 @@ let short = valuesToLinkedListNodes([1, 2]);
 let single = new LinkedListNode(1);
 let long = valuesToLinkedListNodes([1, 2, 3, 4, 5, 6]);
 
-console.log(reverseList(short));
-console.log(reverseList(single));
-console.log(reverseList(long));
-console.log(reverseList(null));
+// console.log(reverseList(short));
+// console.log(reverseList(single));
+// console.log(reverseList(long));
+reverseList(long);
+// console.log(reverseList(null));
 
 //* Helper function to build list from array of node values
 function valuesToLinkedListNodes(values) {
