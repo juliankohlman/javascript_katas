e259276b8bfb238c9d95d70704e8f73e82bcec8d
Julian Kohlman <juliankohlman@gmail.com>
2021-03-01 19:58:07 -0500

    cleans up range kata description

--- 7kyu/getRange.js
+++ 7kyu/getRange.js
@@ -1,14 +1,9 @@
 /**
  * Build a function that can get all the integers between two given numbers.
 
-Example:
+Example: (2,9)
 
-(2,9)
-
-Should give you this output back:
-
-[ 3, 4, 5, 6, 7, 8 ]
-
+Should give you this output back: [ 3, 4, 5, 6, 7, 8 ]
 If startNum is the same as endNum, return an empty array.
  */
 
