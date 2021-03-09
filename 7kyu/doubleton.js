b3f8741801a1fdee43e41af804c4e25007a62ef4
Julian Kohlman <juliankohlman@gmail.com>
2021-03-09 18:16:26 -0500

    doubleton boilerplate code

---
+++ 7kyu/doubleton.js
@@ -0,0 +1,16 @@
+/**
+ * We will call a natural number a "doubleton number" if it contains exactly two distinct digits. For example, 23, 35, 100, 12121 are doubleton numbers, and 123 and 9980 are not.
+
+For a given natural number n (from 1 to 1 000 000), you need to find the next doubleton number. If n itself is a doubleton, return the next bigger than n.
+
+Examples:
+doubleton(120) === 121
+doubleton(1234) === 1311
+doubleton(10) === 12
+ */
+
+const doubleton = (num) => {};
+
+console.log(doubleton(120)); // === 121
+console.log(doubleton(1234)); // === 1311
+console.log(doubleton(10)); // === 12
