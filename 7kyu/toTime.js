18f848fa7c8ce1f49182191b718a085581f0c033
Julian Kohlman <juliankohlman@gmail.com>
2021-03-12 17:04:27 -0500

    using modulus operator to calc hours and minutes

--- 7kyu/toTime.js
+++ 7kyu/toTime.js
@@ -16,7 +16,9 @@ toTime(323500); // => '89 hour(s) and 51 minute(s)'
 
 const toTime = (seconds) => {
   // convert secont into formatted output
-  return seconds / 60;
+  let hours = Math.floor(seconds / 3600);
+  console.log((seconds %= 3600));
+  return hours;
 };
 
 console.log(toTime(3600)); //  "1 hour(s) and 0 minute(s)"
