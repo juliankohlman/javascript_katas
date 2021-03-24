d40b193ae90584f553efd8f4b45329a2dd96a920
Julian Kohlman <juliankohlman@gmail.com>
2021-03-24 17:57:27 -0400

    removes extra comment from divisibleBy

--- 8kyu/divisibleBy.js
+++ 8kyu/divisibleBy.js
@@ -6,7 +6,6 @@ divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
  */
 
 const divisibleBy = (numbers, divisor) => {
-  // solution here
   let res = [];
   numbers.forEach((num) => {
     if (num % divisor === 0) res.push(num);
