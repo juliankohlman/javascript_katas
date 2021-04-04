d0318b0e4b4503ef336bf18d66185adeb0c84f21
Julian Kohlman <juliankohlman@gmail.com>
2021-04-04 16:22:36 -0400

    validate expiry date exercise

---
+++ 7kyu/checkExpiry.js
@@ -0,0 +1,15 @@
+/**
+ * It is a simple, real world task. You will receive a single string as input. It will have the month (2 digits) and year(2 or 4 digits). These are separated by one character ("-" or "/", maybe some spaces too). For example:
+
+02/21
+02 / 21
+02 / 2021
+02-2021
+Assume that all dates are in the XXI century.
+
+Your task is to write a function that returns true or false if the expiry date is not in the past. Note, current month should still return true.
+ */
+
+const checkExpiryValid = (date) => {
+  // solution here
+};
